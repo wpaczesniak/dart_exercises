6f5cc8bb7e0c20e0bab8b8bdfac16922320ff29b
wpaczesniak <wojciech.paczesniak1@gmail.com>
2026-01-04 20:06:33 +0100

    records

--- lib/test.dart
+++ lib/test.dart
@@ -15,4 +15,11 @@ void main() {
   // double -> String
   String piAsString = 3.14159.toStringAsFixed(2);
   assert(piAsString == '3.14');
+  var record = ('first', a: 2, b: true, 'last');
+
+  print(record.$1); // Prints 'first'
+  print(record.a); // Prints 2
+  print(record.b); // Prints true
+  print(record.$2); // Prints 'last'
+
 }
