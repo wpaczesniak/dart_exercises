1d32d8eb98ecf3e2236c9e9abd83d9293d5bd6bf
wpaczesniak <wojciech.paczesniak1@gmail.com>
2026-01-17 19:50:22 +0100

    code unit and code points

--- lib/runes.dart
+++ lib/runes.dart
@@ -4,5 +4,9 @@ void main() {
   var hi = 'Hi 🇩🇰';
   print(hi);
   print('The end of the string: ${hi.substring(hi.length - 1)}');
+  print('The end: ${hi.length}');
   print('The last character: ${hi.characters.last}');
+  print('😆'.runes.length);     // 1 code point
+  print('😆'.length);           // 2 code units
+
 }
