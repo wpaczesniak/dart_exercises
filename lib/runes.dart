31f1566944e69888b93d054683852ebc9846f639
wpaczesniak <wojciech.paczesniak1@gmail.com>
2026-01-20 22:17:47 +0100

    symbols

--- lib/runes.dart
+++ lib/runes.dart
@@ -3,9 +3,9 @@ import 'package:characters/characters.dart';
 void main() {
   var hi = 'Hi 🇩🇰';
   print(hi);
-  print('The end of the string: ${hi.substring(hi.length - 1)}');
-  print('The end: ${hi.length}');
-  print('The last character: ${hi.characters.last}');
+  print('The end of the string: ${hi.substring(hi.length - 1)}'); //� 
+  print('The end: ${hi.length}'); //7
+  print('The last character: ${hi.characters.last}');  //🇩🇰
   print('😆'.runes.length);     // 1 code point
   print('😆'.length);           // 2 code units
   var s = '🇩🇰';
